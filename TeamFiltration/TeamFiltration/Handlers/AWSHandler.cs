fe0ddcd6d3a313dcd76dec15d5863e89574fbcb4
Cyber0P <29807184+Cyber0P@users.noreply.github.com>
2025-10-20 21:30:23 +0200

    Replace FireProx with Normal Proxy

--- TeamFiltration/TeamFiltration/Handlers/AWSHandler.cs
+++ TeamFiltration/TeamFiltration/Handlers/AWSHandler.cs
@@ -8,39 +8,41 @@ using TeamFiltration.Models.TeamFiltration;
 
 namespace TeamFiltration.Handlers
 {
-    public class AWSHandler
-    {
-        /*
+	public class AWSHandler
+	{
+		/*
             All credit to the peeps as Black Hills Information Security, this is simply a C# implementation of their FireProx tool.
             https://github.com/ustayready/fireprox
         */
-        private static GlobalArgumentsHandler _globalProperties { get; set; }
-        private static DatabaseHandler _databaseHandler { get; set; }
-        private static BasicAWSCredentials _basicAWSCredentials { get; set; }
-        private static SessionAWSCredentials _sessionAWSCredentials { get; set; }
-        private static AWSCredentials _AWSCredentials { get; set; }
+		private static GlobalArgumentsHandler _globalProperties { get; set; }
+		private static DatabaseHandler _databaseHandler { get; set; }
+		private static BasicAWSCredentials _basicAWSCredentials { get; set; }
+		private static SessionAWSCredentials _sessionAWSCredentials { get; set; }
+		private static AWSCredentials _AWSCredentials { get; set; }
 
 
-        public async Task<bool> DeleteFireProxEndpoint(string fireProxId, string region)
-        {
-            var amazonAPIGatewayClient = new AmazonAPIGatewayClient(_AWSCredentials, Amazon.RegionEndpoint.GetBySystemName(region));
 
-            Amazon.APIGateway.Model.DeleteRestApiResponse deleteRestApiResponse = await amazonAPIGatewayClient.DeleteRestApiAsync(new Amazon.APIGateway.Model.DeleteRestApiRequest() { RestApiId = fireProxId });
+		// Note: After changes there should not be fireprox used.
+		public async Task<bool> DeleteFireProxEndpoint(string fireProxId, string region)
+		{
+			var amazonAPIGatewayClient = new AmazonAPIGatewayClient(_AWSCredentials, Amazon.RegionEndpoint.GetBySystemName(region));
 
-            if (deleteRestApiResponse.HttpStatusCode == System.Net.HttpStatusCode.Accepted)
-            {
-                _databaseHandler.WriteLog(new Log("FIREPROX", $"Deleted endpoint https://{fireProxId}.execute-api.{region}.amazonaws.com/fireprox/", ""));
+			Amazon.APIGateway.Model.DeleteRestApiResponse deleteRestApiResponse = await amazonAPIGatewayClient.DeleteRestApiAsync(new Amazon.APIGateway.Model.DeleteRestApiRequest() { RestApiId = fireProxId });
 
-                _databaseHandler.DeleteFireProxEndpoint(fireProxId);
+			if (deleteRestApiResponse.HttpStatusCode == System.Net.HttpStatusCode.Accepted)
+			{
+				_databaseHandler.WriteLog(new Log("FIREPROX", $"Deleted endpoint https://{fireProxId}.execute-api.{region}.amazonaws.com/fireprox/", ""));
 
-                return true;
-            }
+				_databaseHandler.DeleteFireProxEndpoint(fireProxId);
+
+				return true;
+			}
 
 
-            return false;
-        }
-        
-        /*
+			return false;
+		}
+
+		/*
         public async Task ListFireProxEndpoint()
         {
             
@@ -52,16 +54,16 @@ namespace TeamFiltration.Handlers
             }
         }
         */
-        public async Task<(Amazon.APIGateway.Model.CreateDeploymentRequest, Models.AWS.FireProxEndpoint)> CreateFireProxEndPoint(string url, string title, string region)
-        {
-            var amazonAPIGatewayClient = new AmazonAPIGatewayClient(_AWSCredentials, Amazon.RegionEndpoint.GetBySystemName(region));
+		public async Task<(Amazon.APIGateway.Model.CreateDeploymentRequest, Models.AWS.FireProxEndpoint)> CreateFireProxEndPoint(string url, string title, string region)
+		{
+			var amazonAPIGatewayClient = new AmazonAPIGatewayClient(_AWSCredentials, Amazon.RegionEndpoint.GetBySystemName(region));
 
-            if (url.EndsWith('/'))
-                url = url.Substring(0, url.Length - 1);
+			if (url.EndsWith('/'))
+				url = url.Substring(0, url.Length - 1);
 
-            string versionDate = DateTime.UtcNow.ToString("yyyy-MM-ddTHH:mm:ssZ");
+			string versionDate = DateTime.UtcNow.ToString("yyyy-MM-ddTHH:mm:ssZ");
 
-            string template = @"
+			string template = @"
         {
           ""swagger"": ""2.0"",
           ""info"": {
@@ -154,75 +156,75 @@ namespace TeamFiltration.Handlers
             }
           }
         }";
-            template = template.Replace("{{url}}", url);
-            template = template.Replace("{{title}}", "teamfiltration_fireprox_" + title);
-            template = template.Replace("{{version_date}}", versionDate);
+			template = template.Replace("{{url}}", url);
+			template = template.Replace("{{title}}", "teamfiltration_fireprox_" + title);
+			template = template.Replace("{{version_date}}", versionDate);
 
-            var templateBytes = Encoding.UTF8.GetBytes(template);
+			var templateBytes = Encoding.UTF8.GetBytes(template);
 
-            var importRestApiAsyncResponse = await amazonAPIGatewayClient.ImportRestApiAsync(
-                new Amazon.APIGateway.Model.ImportRestApiRequest()
-                {
-                    Parameters = new Dictionary<string, string> {
-                        { "endpointConfigurationTypes", "REGIONAL" }
+			var importRestApiAsyncResponse = await amazonAPIGatewayClient.ImportRestApiAsync(
+				new Amazon.APIGateway.Model.ImportRestApiRequest()
+				{
+					Parameters = new Dictionary<string, string> {
+						{ "endpointConfigurationTypes", "REGIONAL" }
 
-                    },
-                    Body = new System.IO.MemoryStream(templateBytes)
-                }
-            );
+					},
+					Body = new System.IO.MemoryStream(templateBytes)
+				}
+			);
 
-            Amazon.APIGateway.Model.CreateDeploymentRequest createDeploymentRequest = new Amazon.APIGateway.Model.CreateDeploymentRequest()
-            {
-                Description = "TeamFiltration FireProx Prod",
-                StageDescription = "TeamFiltration FireProx Prod Stage",
-                StageName = "fireprox",
-                RestApiId = importRestApiAsyncResponse.Id
-            };
+			Amazon.APIGateway.Model.CreateDeploymentRequest createDeploymentRequest = new Amazon.APIGateway.Model.CreateDeploymentRequest()
+			{
+				Description = "TeamFiltration FireProx Prod",
+				StageDescription = "TeamFiltration FireProx Prod Stage",
+				StageName = "fireprox",
+				RestApiId = importRestApiAsyncResponse.Id
+			};
 
-            Amazon.APIGateway.Model.CreateDeploymentResponse createDeploymentAsyncResponse = await amazonAPIGatewayClient.CreateDeploymentAsync(createDeploymentRequest);
-
-
-            _databaseHandler.WriteLog(new Log("FIREPROX", $"Created endpoint https://{createDeploymentRequest.RestApiId}.execute-api.{region}.amazonaws.com/fireprox/", ""));
-
-            var fireproxEndpoint = new Models.AWS.FireProxEndpoint()
-            {
-                Deleted = false,
-                Active = true,
-                URL = url,
-                FireProxURL = $"https://{createDeploymentRequest.RestApiId}.execute-api.{region}.amazonaws.com/fireprox/",
-                Region = region,
-                RestApiId = createDeploymentRequest.RestApiId
-            };
-
-            _databaseHandler.WriteFireProxEndpoint(fireproxEndpoint);
+			Amazon.APIGateway.Model.CreateDeploymentResponse createDeploymentAsyncResponse = await amazonAPIGatewayClient.CreateDeploymentAsync(createDeploymentRequest);
 
 
-            return (createDeploymentRequest, fireproxEndpoint);
-        }
+			_databaseHandler.WriteLog(new Log("FIREPROX", $"Created endpoint https://{createDeploymentRequest.RestApiId}.execute-api.{region}.amazonaws.com/fireprox/", ""));
 
-        public AWSHandler(string AWSAccessKey, string AWSSecretKey, string AWSSessionToken, DatabaseHandler databaseHandler)
-        {
-            _databaseHandler = databaseHandler;
+			var fireproxEndpoint = new Models.AWS.FireProxEndpoint()
+			{
+				Deleted = false,
+				Active = true,
+				URL = url,
+				FireProxURL = $"https://{createDeploymentRequest.RestApiId}.execute-api.{region}.amazonaws.com/fireprox/",
+				Region = region,
+				RestApiId = createDeploymentRequest.RestApiId
+			};
 
-            if (!string.IsNullOrEmpty(AWSAccessKey) && !string.IsNullOrEmpty(AWSSecretKey) && string.IsNullOrEmpty(AWSSessionToken))
-            {
+			_databaseHandler.WriteFireProxEndpoint(fireproxEndpoint);
 
-                _basicAWSCredentials = new BasicAWSCredentials(
-                    AWSAccessKey,
-                    AWSSecretKey
-                    );
-                _AWSCredentials = _basicAWSCredentials;
-            }
-            else if (!string.IsNullOrEmpty(AWSAccessKey) && !string.IsNullOrEmpty(AWSSecretKey) && !string.IsNullOrEmpty(AWSSessionToken))
-            {
-                _sessionAWSCredentials = new SessionAWSCredentials(
-                    AWSAccessKey,
-                    AWSSecretKey,
-                    AWSSessionToken
-                    );
-                _AWSCredentials = _sessionAWSCredentials;
-            }
 
-        }
-    }
+			return (createDeploymentRequest, fireproxEndpoint);
+		}
+
+		public AWSHandler(string AWSAccessKey, string AWSSecretKey, string AWSSessionToken, DatabaseHandler databaseHandler)
+		{
+			_databaseHandler = databaseHandler;
+
+			if (!string.IsNullOrEmpty(AWSAccessKey) && !string.IsNullOrEmpty(AWSSecretKey) && string.IsNullOrEmpty(AWSSessionToken))
+			{
+
+				_basicAWSCredentials = new BasicAWSCredentials(
+					AWSAccessKey,
+					AWSSecretKey
+					);
+				_AWSCredentials = _basicAWSCredentials;
+			}
+			else if (!string.IsNullOrEmpty(AWSAccessKey) && !string.IsNullOrEmpty(AWSSecretKey) && !string.IsNullOrEmpty(AWSSessionToken))
+			{
+				_sessionAWSCredentials = new SessionAWSCredentials(
+					AWSAccessKey,
+					AWSSecretKey,
+					AWSSessionToken
+					);
+				_AWSCredentials = _sessionAWSCredentials;
+			}
+
+		}
+	}
 }
